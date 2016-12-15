4282148de0bddfa153c428810166ec4ce2bd619d
sandip <sandipgh19@gmail.com>
2016-12-15 17:56:30 +0530

    update

--- app/src/main/java/com/example/sandip/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/sandip/speechtotext/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.sandip.speechtotext;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    protected static final int RESULT_SPEECH = 1;
+    private ImageButton btnSpeak;
+    private TextView txtText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
