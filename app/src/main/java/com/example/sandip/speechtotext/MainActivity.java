1da71225ab2d1161b5961608362e7d47f88a68cc
sandip <sandipgh19@gmail.com>
2016-12-15 18:04:28 +0530

    update

--- app/src/main/java/com/example/sandip/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/sandip/speechtotext/MainActivity.java
@@ -24,5 +24,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtText = (TextView) findViewById(R.id.txtText);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    txtText.setText("");
+                }
+                catch (ActivityNotFoundException a) {
+                    Toast t = Toast.makeText(getApplicationContext(), "Opps! Your device doesn't support Speech to Text", Toast.LENGTH_SHORT);
+                    t.show();
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH:
+                {
+                    if (resultCode == RESULT_OK && null != data) {
+                        ArrayList<String> text = data .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        txtText.setText(text.get(0));
+                    }
+                    break;
+                }
+            }
+        }
 }
